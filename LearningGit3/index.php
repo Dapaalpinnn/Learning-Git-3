b094d8fa598dcae01ae3b03a48b18a310ef0865a
Dafa Alvin <dafaalvin10@gmail.com>
2024-03-13 08:59:05 +0700

    Menambahkan list

--- LearningGit3/index.php
+++ LearningGit3/index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque accusantium ipsum minima eius at deleniti alias, voluptas nesciunt. Consectetur sapiente illum ut odio ducimus placeat aliquid temporibus hic, eligendi numquam!</p>
+    <ol>
+        <li>Setiawan Putra</li>
+        <li>Doddy Pratama</li>
+    </ol>
 </body>
 </html>
